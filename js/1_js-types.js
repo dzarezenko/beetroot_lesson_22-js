6096a3a4157de93a9e895dc4a02d7c4f8bc9e334
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2019-10-28 20:28:57 +0200

    JS data types

---
+++ js/1_js-types.js
@@ -0,0 +1,43 @@
+/*
+  Number
+  String
+  Boolean
+  null
+  undefined
+  Symbol
+*/
+
+let x, y;
+
+let a = 7;
+let b = 3;
+
+let c = a + b;
+let d = a / b;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+let firstName = "Irina";
+let lastName = 'Ivanova';
+let fullName1 = `First Name: ${firstName}, Last Name: ${lastName}, ${a}`;
+let fullName2 = "First Name: " + firstName + ", Last Name: " + lastName + ", " + a;
+
+let description = `The description is "${name}"`;
+
+console.log(fullName1);
+console.log(fullName2);
+
+let x = (a != b);
+let y = false;
+
+/**
+ "number"
+ "string"
+ "boolean"
+ "object"
+ "symbol"
+ "function"
+ "undefined"
+ */

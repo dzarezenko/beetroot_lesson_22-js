9d3c1fb4d2f007626f5664043bec778e3650300f
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2019-11-23 11:38:44 +0200

    Objects example

--- js/hw/currencies.js
+++ js/hw/currencies.js
@@ -6,19 +6,25 @@
 let amount = parseInt(prompt("Enter an amount"))
 let currency = prompt("Enter a currency")
 
-const TITLE_EURO = "EUR";
-const TITLE_GRIVNA = "UAN";
-const TITLE_MANAT = "AZN";
-const RATE_EUR = 0.90;
-const RATE_UAH = 24.81;
-const RATE_AZN = 1.70;
+const EURO = {
+  title: "EUR",
+  rate: 0.9,
+};
+const HRIVNA = {
+  title: "UAH",
+  rate: 24.15,
+};
+const MANAT = {
+  title: "AZN",
+  rate: 1.7,
+};
 
-if (currency == TITLE_EURO ) {
-    alert(amount * RATE_EUR)
-} else if (currency == TITLE_GRIVNA) {
-    alert(amount * RATE_UAH)
-} else if (currency == TITLE_MANAT) {
-    alert(amount * RATE_AZN)
+if (currency == EURO.title ) {
+    alert(amount * EURO.rate)
+} else if (currency == HRIVNA.title) {
+    alert(amount * HRIVNA.rate)
+} else if (currency == MANAT.title) {
+    alert(amount * MANAT.rate)
 } else if (currency != "" ) {
     alert("We do not have this currency")
 } 
